15ef0805066ba055a74197dbfead7bdbd4697a58
anke <245783065@qq.com>
2024-11-26 18:04:14 +0800

    feat: 新增window属性扩展

---
+++ app/global.d.ts
@@ -0,0 +1,4 @@
+// global.d.ts
+interface Window {
+  blobity: unknown; // 或者你可以使用更具体的类型，取决于 blobity 的实现
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+import useBlobity from "blobity/lib/react/useBlobity";
+import { useEffect } from "react";
 import PreLoader from "./animations/PreLoader/PreLoader";
 import Hero from "./hero-section/Hero";
+import { initialBlobityOptions } from "./utils/BlobityConfig";
 
 export default function Home() {
+  const blobityInstance = useBlobity(initialBlobityOptions);
+
+  useEffect(() => {
+    if (blobityInstance.current) {
+      window.blobity = blobityInstance.current;
+    }
+  }, [blobityInstance]);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  }, []);
+
   return (
     <>
       <PreLoader />
